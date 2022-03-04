b7639861431300e5cfb19559dce91f6b7b009086
Christian Kubelle <ckubelle@stevens.edu>
2022-03-04 15:19:18 -0500

    Initial Remote Push

--- MagicMirror/config/config.js
+++ MagicMirror/config/config.js
@@ -8,7 +8,8 @@
  * and https://docs.magicmirror.builders/modules/configuration.html
  */
 let config = {
-	address: "localhost", 	// Address to listen on, can be:
+	address: "0.0.0.0", 	// Christian - For remote control to work thsi must be 0.0.0.0 if this causes problems let me know
+							// Address to listen on, can be:
 							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
@@ -16,12 +17,14 @@ let config = {
 	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
 					// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "10.156.74.3"], 	// Set [] to allow all IP addresses
 															// or add a specific IPv4 of 192.168.1.5 :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
+	/* Christian - To use remote control - add your specific ip address from the device you want to connect to - I have my Iphone IP starting with 10. as an example*/
+	
 	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
 	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
 	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
@@ -61,6 +64,21 @@ let config = {
 				]
 			},
 			classes: 'essential'
+		},
+		{
+			module: 'MMM-Remote-Control',
+			// uncomment the following line to show the URL of the remote control on the mirror
+			 position: 'bottom_left',
+			// you can hide this module afterwards from the remote control itself
+			config: {
+				customCommand: {},  // Optional, See "Using Custom Commands" below
+				showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+				secureEndpoints: true, // Optional, See API/README.md
+				// uncomment any of the lines below if you're gonna use it
+				// customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+				// apiKey: "", // Optional, See API/README.md for details
+				// classes: {} // Optional, See "Custom Classes" below
+			}
 		},
         {
             module: "MMM-NJTransit",
@@ -115,7 +133,7 @@ let config = {
 			},
 			classes: 'essential'
 		},
-
+		/*
 		{
 			module: "MMM-Canvas",
 			position: "bottom_left",
@@ -129,6 +147,7 @@ let config = {
 			},
 			classes: 'academic'
 		},
+		*/
 		{
 			module: "MMM-NowPlayingOnSpotify",
 			position: "bottom_right",
